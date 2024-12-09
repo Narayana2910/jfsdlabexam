9e1a6c537a43f315455d7ca05f93cb7ce5e88e43
Narayana2910 <2200031446@kluniversity.in>
2024-12-09 21:05:03 +0530

    Add files via upload

---
+++ src/main/java/com/klef/jfsd/exam/jfsdendlab/App.java
@@ -0,0 +1,10 @@
+package com.klef.jfsd.exam.jfsdendlab;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
